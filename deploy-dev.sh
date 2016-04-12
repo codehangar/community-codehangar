6c28a48983933a62b8c5b2a224a5a264cb01c98a
Ian Grail <grailians@gmail.com>
2016-04-12 10:33:02 -0400

    chore: try and get travis to deploy via ssh

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,9 +3,13 @@
 # Arg 1 = Reponame
 # Arg 2 = Rev number
 
-REPONAME=$1
-REVISION=$2
-REV_NAME=$REPONAME-$REVISION
+if [ "$TRAVIS_BRANCH" != "master" ]; then
+  exit 1
+fi
+
+REPONAME=${TRAVIS_REPO_SLUG}
+REVISION=${TRAVIS_BUILD_NUMBER}
+REV_NAME=${REPONAME}-${REVISION}
 echo "REV_NAME:" ${REV_NAME}
 
 # npm install
@@ -18,11 +22,11 @@ echo "REV_NAME:" ${REV_NAME}
 tar -C . -cvf ${REV_NAME}.tar .
 
 # Create /var/www directory if not exists
-ssh root@datgoat.com "mkdir -p /var/www/${REV_NAME};"
+sshpass -e ssh root@datgoat.com "mkdir -p /var/www/${REV_NAME};"
 
 # Transfer tarball
 # scp artifacts/${REV_NAME}.tar root@datgoat.com:/var/www/${REV_NAME}.tar
-scp ${REV_NAME}.tar root@datgoat.com:/var/www/${REV_NAME}.tar
+sshpass -e scp ${REV_NAME}.tar root@datgoat.com:/var/www/${REV_NAME}.tar
 if [ $? -ne 0 ]; then
   echo "Tarball Transfer Failed"
   exit $?
@@ -32,7 +36,7 @@ fi
 # Backup current package
 # Position new package to be served
 # Remove the backup
-ssh root@datgoat.com "
+sshpass -e ssh root@datgoat.com "
   mkdir -p /var/www/${REV_NAME};
   mkdir -p /var/www/${REPONAME};
   tar -xf /var/www/${REV_NAME}.tar -C /var/www/${REV_NAME};
